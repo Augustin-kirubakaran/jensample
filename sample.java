7c73d25ec80e4cfa015004c3bb6315a0132219c6
Augustin29082002 <23203004@srcas.ac.in>
2024-07-01 11:57:59 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,9 +1,9 @@
 class sample{
 public static void main(String[]args)
 {
-system.out.println("hello")
-system.out.println("my Name is")
-system.out.println("Augustin")
-system.out.println("welcome")
+system.out.println("hello");
+system.out.println("my Name is");
+system.out.println("Augustin");
+system.out.println("welcome");
   }
 }
